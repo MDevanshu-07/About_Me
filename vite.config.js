ae5b36c1b834f0dbf2054249f06b66c0b935c69e
MDevanshu-07 <devanshumattam1405@gmail.com>
2026-09-15 18:34:44 +0530

    Changes in config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.Vite_Base_Path || "/https://github.com/MDevanshu-07/About_Me"
 })
